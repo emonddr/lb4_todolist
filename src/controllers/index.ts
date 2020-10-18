c68103d94408a6322848f7854a2991a530672550
Dominique Emond <dremond@ca.ibm.com>
2020-10-17 21:15:29 -0400

    first commit

---
+++ src/controllers/index.ts
@@ -0,0 +1,6 @@
+export * from './ping.controller';
+export * from './todo-list-image-todo-list.controller';
+export * from './todo-list-todo-list-image.controller';
+export * from './todo-list-todo.controller';
+export * from './todo-list.controller';
+export * from './todo.controller';
---
+++ src/models/todo-list.model.ts
@@ -0,0 +1,44 @@
+import {Entity, hasMany, hasOne, model, property} from '@loopback/repository';
+import {
+  TodoListImage,
+  TodoListImageWithRelations,
+} from './todo-list-image.model';
+import {Todo, TodoWithRelations} from './todo.model';
+
+@model()
+export class TodoList extends Entity {
+  @property({
+    type: 'number',
+    id: true,
+    generated: false,
+  })
+  id?: number;
+
+  @property({
+    type: 'string',
+  })
+  title?: string;
+
+  @property({
+    type: 'string',
+  })
+  color?: string;
+
+  @hasMany(() => Todo)
+  todos: Todo[];
+
+  @hasOne(() => TodoListImage)
+  image: TodoListImage;
+
+  constructor(data?: Partial<TodoList>) {
+    super(data);
+  }
+}
+
+export interface TodoListRelations {
+  // describe navigational properties here
+  todos?: TodoWithRelations[];
+  image?: TodoListImageWithRelations;
+}
+
+export type TodoListWithRelations = TodoList & TodoListRelations;
